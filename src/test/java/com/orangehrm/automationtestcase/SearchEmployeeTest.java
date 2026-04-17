ac691fc07b48c7b963e406e48cc8e801151cb8e6
Shiva Prasad <shivaprasadreddy32@gmail.com>
2026-04-17 18:57:21 +0530

    Added dataProvider

--- src/test/java/com/orangehrm/automationtestcase/SearchEmployeeTest.java
+++ src/test/java/com/orangehrm/automationtestcase/SearchEmployeeTest.java
@@ -1,5 +1,6 @@
 package com.orangehrm.automationtestcase;
 
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import com.orangehrm.basic_utilites.BaseClass;
@@ -8,25 +9,29 @@ import com.orangehrm.objectrepositry_utilites.PimPage;
 
 public class SearchEmployeeTest extends BaseClass {
 
-	@Test
-	public void SearchIngEmployee()
-	{
-		HomePage hm=new HomePage(driver);
+	@Test(dataProvider = "getData")
+	public void searchEmployee(String name, int id) {
+		HomePage hm = new HomePage(driver);
 		hm.ClickOnPimElement();
-		PimPage pm=new PimPage(driver);
-		pm.searchEmpById();
-		System.out.println("Shivu "+" Employee Found");
+		PimPage pim = new PimPage(driver);
+		pim.searchEmpById();
+		System.out.println(name + " Found With Id :" + id);
 	}
-	
+
 	@Test
-	public void searchIngEmployee()
-	{
-		HomePage hm=new HomePage(driver);
-		hm.ClickOnPimElement();
-		PimPage pm=new PimPage(driver);
-		pm.searchEmpById();
-		System.out.println("Employee Name :Shivu and Id is 9999:");
+	@DataProvider
+	public Object[][] getData() {
+		Object obj[][] = new Object[3][2];
+		obj[0][0] = "Shiv";
+		obj[0][1] = 999;
+
+		obj[1][0] = "Raghu";
+		obj[1][1] = 88;
+
+		obj[2][0] = "Harsha";
+		obj[2][1] = 55;
+
+		return obj;
 	}
-	
-	
+
 }
